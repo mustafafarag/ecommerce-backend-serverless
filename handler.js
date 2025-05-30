1c66ba3b8a33db4ab6e296ebbf3382cfe3155efb
mustafafarag <mustafafarag23@gmail.com>
2025-05-30 06:14:29 +0300

    📝 Refactor handler to include initialization for MongoDB and Redis connections (// To take advantage of Lambda Execution Context Reuse for warm starts/performance )

--- handler.js
+++ handler.js
@@ -1,6 +1,29 @@
 const serverless = require('serverless-http')
 const dotenv = require('dotenv')
+const dbConnect = require('./config/dbConnect')
+const connectRedis = require('./config/redis')
+const app = require('./index')
+
+// Load environment variables from .env file
 dotenv.config()
 
-const app = require('./index')
-module.exports.handler = serverless(app)
+// To take advantage of Lambda Execution Context Reuse for warm starts/performance 
+let isInitialized = false
+
+const setup = async () => {
+  if (!isInitialized) {
+    await Promise.all([
+      dbConnect(),    // MongoDB
+      connectRedis()  // Redis
+    ])
+    isInitialized = true
+    console.log('✅ MongoDB Atlas and Redis connected')
+  }
+}
+
+const handler = serverless(app)
+
+module.exports.handler = async (event, context) => {
+  await setup()
+  return handler(event, context)
+}
